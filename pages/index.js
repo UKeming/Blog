cb49c9e5d6c09ba7c22d78629c95b964400fa737
Keming <ukeming@icloud.com>
2023-03-10 16:58:43 -0700

    fix error

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import { Inter } from 'next/font/google'
-import { Navigator } from './navigator'
-import { Footer } from './footer'
-import { Introduction } from './body/introduction'
-import { Post } from './body/post'
-import { Contact } from './body/contact'
+import { Navigator } from '../components/navigator'
+import { Footer } from '../components/footer'
+import { Introduction } from '../components/body/introduction'
+import { Post } from '../components/body/post'
+import { Contact } from '../components/body/contact'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
-import { routes } from './routes'
+import { routes } from '../components/routes'
 
 const inter = Inter({ subsets: ['latin'] })
 
