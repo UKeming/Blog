5f2affd1e76c2d250213e81a0365fd504640a6c3
Keming <ukeming@icloud.com>
2023-03-10 22:53:21 -0700

    add logo

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Keming&lsquos Blog</title>
+        <title>Keming&lsquos; Blog</title>
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
